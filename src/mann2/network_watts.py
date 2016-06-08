9dcf819a9c5e616bba8a38ce4b5bd578e98f6d54
Daniel Chen <Maveri2k@yahoo.com>
2016-06-08 16:28:43 -0400

    changes to how the agents are stored in the Graph
    the network graph has agent id as a key.
    The key will return another dict where the key is 'agent', and the value
    is the actual AgentWatts class

--- src/mann2/network_watts.py
+++ src/mann2/network_watts.py
@@ -10,26 +10,30 @@ class NetworkWatts(network.Network):
     def __init__(self, config, logger):
         super(NetworkWatts, self).__init__(config, logger)
 
-        print(self.config)
-
     def create_agents_in_graph(self):
         self.logger.info('Adding agents into graph')
         num_agents = int(self.config['single_sim']['num_agents'])
-        all_agents = {}
+        # self.all_agents = {}
         for _ in range(num_agents):
             new_agent = agent_watts.AgentWatts(self.config, self.logger)
-            self.graph.add_node(new_agent)
+
+            self.graph.add_node(new_agent.agent_id, agent=new_agent)
             self.logger.debug(
                 'Agent {} added to graph'.format(new_agent.agent_id))
 
-            all_agents[new_agent.agent_id] = new_agent
-            self.logger.debug(
-                'Agent {} added to dict of all_agents'.format(new_agent.agent_id))
+            # self.all_agents[new_agent.agent_id] = new_agent
+            # self.logger.debug(
+            #     'Agent {} added to dict of all_agents'.format(new_agent.agent_id))
+        return(self)
 
     def generate_graph_network(self):
         self.logger.debug("Connecting agents into network")
-            self.graph.add_edge(all_agents[u], all_agents[v])
-        nx.draw_circular(self.graph)
-        plt.show()
         for u, v in self.nx_graph.edges_iter():
+            self.graph.add_edge(u, v)
+            if bool(self.config['graph']['force_directed']):
+                self.logger.info('Forcing direction by adding reverse edges')
+                self.graph.add_edge(v, u)
+        # nx.draw_circular(self.graph)
+        # plt.show()
         self.logger.info("Network Graph of agents created.")
+        return(self)
--- src/mann2/run_model_watts.py
+++ src/mann2/run_model_watts.py
@@ -1,3 +1,6 @@
+from random import choice
+import networkx as nx
+
 import model_watts
 
 
@@ -7,7 +10,16 @@ def main():
 
     # setup model
     model.setup_model_graph()
-    model.setup_model_run()
+
+    # TODO unit test this check
+    # selected_agent_id = choice(model.network.graph.nodes())
+    # print(model.network.graph.nodes())
+    # print('selected_agent_id: {}'.format(selected_agent_id))
+    # print(nx.info(model.network.graph))
+    # print(model.network.graph.node[selected_agent_id])
+    # print(type(model.network.graph.node[selected_agent_id]['agent']))
+
+    # model.setup_model_run()
 
 if __name__ == '__main__':
     main()
